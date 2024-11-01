99ce1e3dc53e72cf1673a76d112e0345fe2cefd7
dida0423 <didak0423@gmail.com>
2024-11-01 16:19:56 +0900

    Sample Animation Scene + Button

---
+++ Assets/Scripts/AnimatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenCloseMouth()
+    {
+        if (animator.GetBool("mouthOpen_b")) animator.SetBool("mouthOpen_b", false);
+        else animator.SetBool("mouthOpen_b", true);
+    }
+}
