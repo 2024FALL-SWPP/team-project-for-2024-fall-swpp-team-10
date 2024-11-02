29d2a6c8058bb55106e1fe9ab6d19e561daa387c
dida0423 <didak0423@gmail.com>
2024-11-02 16:58:38 +0900

    Add Color Change
    Color changing script (not gradual but discrete)

--- Assets/Scripts/AnimatorTest.cs
+++ Assets/Scripts/AnimatorTest.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class AnimatorTest : MonoBehaviour
 {
     public Animator animator;
+    int col;
+    Color[] myColors = new Color[] { Color.white, Color.red, Color.gray };
 
     // Start is called before the first frame update
     void Start()
     {
+        col = 0;
+        ColorUtility.TryParseHtmlString("#CB5353", out myColors[1]);
     }
 
     // Update is called once per frame
@@ -21,5 +25,18 @@ public class AnimatorTest : MonoBehaviour
     {
         if (animator.GetBool("mouthOpen_b")) animator.SetBool("mouthOpen_b", false);
         else animator.SetBool("mouthOpen_b", true);
+    }
+
+    public void ChangeColor()
+    {
+        col = (col + 1) % 3;
+        foreach (Transform childTransform in gameObject.transform)
+        {
+            GameObject child = childTransform.gameObject;
+            SkinnedMeshRenderer smr = child.GetComponent<SkinnedMeshRenderer>();
+            if (smr != null) smr.material.color = myColors[col];
+            MeshRenderer mr = child.GetComponent<MeshRenderer>();
+            if (mr != null) mr.material.color = myColors[col];
+        }
     }
 }
