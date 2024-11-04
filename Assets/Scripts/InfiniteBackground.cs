10bf07103f75340f9e2ea9ca2e42dbafd3277be7
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2024-11-04 23:28:01 +0900

    Infinite Background 구현
    speed는 script에서 조절해야함

---
+++ Assets/Scripts/InfiniteBackground.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour
+{
+    private float speed = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+        if (transform.position.z < -240)
+        {
+            transform.Translate(transform.position.x, transform.position.y, 480);
+        }
+    }
+}
