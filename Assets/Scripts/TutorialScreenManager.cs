41c21cec63df96d4f3d738f9d25e7ea39dbbaaa7
ysj720 <danny020720@gmail.com>
2024-11-05 23:30:58 +0900

    피드백 반영
    Circle.png 위치 옮김
    각 폴더 및 파일명 수정
    text를 image로 변경
    버튼 사이즈 및 select player와 tutorial 사이 거리 조정

--- Assets/Scripts/TutorialScreenManager.cs
+++ Assets/Scripts/TutorialScreenManager.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 
 public class TutorialScreenManager : MonoBehaviour
 {
-    public TextMeshProUGUI[] tutorialTexts;  // Text 대신 TextMeshProUGUI 배열로 설정
+    public Image[] tutorialPages;  // 각 페이지를 Image로 변경
     public Button previousButton;
     public Button nextButton;
 
@@ -15,21 +14,21 @@ public class TutorialScreenManager : MonoBehaviour
     void Start()
     {
         UpdateTutorialPage();
-        //previousButton.onClick.AddListener(GoToPreviousPage);
-        //nextButton.onClick.AddListener(GoToNextPage);
+        previousButton.onClick.AddListener(GoToPreviousPage);
+        nextButton.onClick.AddListener(GoToNextPage);
     }
 
     void UpdateTutorialPage()
     {
-        // 모든 페이지 텍스트 비활성화
-        for (int i = 0; i < tutorialTexts.Length; i++)
+        // 모든 페이지 비활성화
+        for (int i = 0; i < tutorialPages.Length; i++)
         {
-            tutorialTexts[i].gameObject.SetActive(i == currentPage);
+            tutorialPages[i].gameObject.SetActive(i == currentPage);
         }
 
-        // 현재 페이지에 맞는 텍스트만 활성화
+        // 현재 페이지 활성화
         previousButton.interactable = currentPage > 0;
-        nextButton.interactable = currentPage < tutorialTexts.Length - 1;
+        nextButton.interactable = currentPage < tutorialPages.Length - 1;
     }
 
     public void GoToPreviousPage()
@@ -43,7 +42,7 @@ public class TutorialScreenManager : MonoBehaviour
 
     public void GoToNextPage()
     {
-        if (currentPage < tutorialTexts.Length - 1)
+        if (currentPage < tutorialPages.Length - 1)
         {
             currentPage++;
             UpdateTutorialPage();
