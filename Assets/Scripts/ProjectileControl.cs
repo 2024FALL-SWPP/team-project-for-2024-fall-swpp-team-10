a7df50faae72aabc40cb50a4a31f6d50f922d11f
dida0423 <didak0423@gmail.com>
2024-11-08 14:24:36 +0900

    Side to side movement + Carrot Shooting

---
+++ Assets/Scripts/ProjectileControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileControl : MonoBehaviour
+{
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsObjectInView(this.transform.position))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    bool IsObjectInView(Vector3 objectPosition)
+    {
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(objectPosition);
+
+        return viewportPosition.x >= 0 && viewportPosition.x <= 1 &&
+               viewportPosition.y >= 0 && viewportPosition.y <= 1 &&
+               viewportPosition.z > 0;
+    }
+}
