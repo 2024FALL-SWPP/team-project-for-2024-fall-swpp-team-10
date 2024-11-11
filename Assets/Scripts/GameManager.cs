15f627fa974181162aa4fabeaa99f0f7059f97ca
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2024-11-11 20:52:42 +0900

    Debug Log 제거

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(playerName);
+
     }
 
     public void LoadMainMenu()
