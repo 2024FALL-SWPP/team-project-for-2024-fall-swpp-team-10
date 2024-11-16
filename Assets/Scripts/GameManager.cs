b63d26435e5ae91aadfd16e69a49a8edbd938ad2
Minjoon Choi <miles@dunamu.com>
2024-11-16 22:36:27 +0900

    prevent score from becoming negative

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -121,6 +121,7 @@ public class GameManager : MonoBehaviour
     public void AddScore(int _score)
     {
         score += _score;
+        if (score < 0) score = 0;
     }
     public int GetScore()
     {
