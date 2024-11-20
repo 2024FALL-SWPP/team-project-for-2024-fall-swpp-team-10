c28051e8073cc0af786170c97637b339258be6a9
Jaeun Park <wodjs2640@naver.com>
2024-11-20 20:50:21 +0900

    리더보드 정리(안됨)

--- Assets/Scripts/LeaderBoardManager.cs
+++ Assets/Scripts/LeaderBoardManager.cs
@@ -13,8 +13,9 @@ public class LeaderBoardManager : MonoBehaviour
     private TextMeshProUGUI myID; //현재 ID
 
     [Header("Rank Info")]
-    public GameObject[] rankText = new GameObject[5];
+    public GameObject[] rankIDText = new GameObject[5];
     private TextMeshProUGUI[] rankID = new TextMeshProUGUI[5]; //보이는 ID
+    public GameObject[] rankScoreText = new GameObject[5];
     private TextMeshProUGUI[] rankScore = new TextMeshProUGUI[5]; //보이는 점수
 
     private int[] savedRankScore = new int[7]; //저장된 점수
@@ -25,9 +26,13 @@ public class LeaderBoardManager : MonoBehaviour
     {
         Rank();
         myScore = myScoreText.GetComponent<TextMeshProUGUI>();
-        myScore.text = GameManager.inst.GetScore().ToString();
         myID = myIDText.GetComponent<TextMeshProUGUI>();
-        myID.text = GameManager.inst.GetPlayerName();
+        for (int i = 0; i < rankIDText.Length; i++)
+        {
+            rankID[i] = rankIDText[i].GetComponent<TextMeshProUGUI>();
+            rankScore[i] = rankScoreText[i].GetComponent<TextMeshProUGUI>();
+        }
+        RankSet();
     }
 
     // Update is called once per frame
@@ -38,7 +43,12 @@ public class LeaderBoardManager : MonoBehaviour
 
     void RankSet()  //랭킹 점수 불러오기
     {
-
+        myScore.text = GameManager.inst.GetScore().ToString();
+        myID.text = GameManager.inst.GetPlayerName();
+        Score_first_i = PlayerPrefs.GetInt("1st");
+        Score_second_i = PlayerPrefs.GetInt("2nd");
+        Score_third_i = PlayerPrefs.GetInt("3rd");
+        Score_now_i = PlayerPrefs.GetInt("Score");
     }
 
     void Rank()  //현재점수와 랭킹점수 비교 및 배치 함수
@@ -59,8 +69,15 @@ public class LeaderBoardManager : MonoBehaviour
 
     void RankSave()   //종료전 랭킹 점수 저장
     {
-        // PlayerPrefs.SetInt("1st", Score_first_i);
-        // PlayerPrefs.SetInt("2nd", Score_second_i);
-        // PlayerPrefs.SetInt("3rd", Score_third_i);
+        PlayerPrefs.SetInt("1st", savedRankScore[0]);
+        PlayerPrefs.SetString("1st", savedRankID[0]);
+        PlayerPrefs.SetInt("2nd", savedRankScore[0]);
+        PlayerPrefs.SetString("2nd", savedRankID[0]);
+        PlayerPrefs.SetInt("3rd", savedRankScore[0]);
+        PlayerPrefs.SetString("3rd", savedRankID[0]);
+        PlayerPrefs.SetInt("4th", savedRankScore[0]);
+        PlayerPrefs.SetString("4th", savedRankID[0]);
+        PlayerPrefs.SetInt("5th", savedRankScore[0]);
+        PlayerPrefs.SetString("5th", savedRankID[0]);
     }
 }
