519aaae0e7d4bbd276e1808138df805413d38f75
Jaeun Park <wodjs2640@naver.com>
2024-11-20 17:30:05 +0900

    리더보드 정리(임시)

--- Assets/Scripts/LeaderBoardManager.cs
+++ Assets/Scripts/LeaderBoardManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,14 +6,24 @@ using UnityEngine;
 
 public class LeaderBoardManager : MonoBehaviour
 {
+    [Header("My Info")]
     public GameObject myScoreText;
-    private TextMeshProUGUI myScore;
+    private TextMeshProUGUI myScore; //현재 점수
     public GameObject myIDText;
-    private TextMeshProUGUI myID;
+    private TextMeshProUGUI myID; //현재 ID
+
+    [Header("Rank Info")]
+    public GameObject[] rankText = new GameObject[5];
+    private TextMeshProUGUI[] rankID = new TextMeshProUGUI[5]; //보이는 ID
+    private TextMeshProUGUI[] rankScore = new TextMeshProUGUI[5]; //보이는 점수
+
+    private int[] savedRankScore = new int[7]; //저장된 점수
+    private string[] savedRankID = new string[7]; //저장된 ID
 
     // Start is called before the first frame update
     void Awake()
     {
+        Rank();
         myScore = myScoreText.GetComponent<TextMeshProUGUI>();
         myScore.text = GameManager.inst.GetScore().ToString();
         myID = myIDText.GetComponent<TextMeshProUGUI>();
@@ -23,5 +34,33 @@ public class LeaderBoardManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void RankSet()  //랭킹 점수 불러오기
+    {
+
+    }
+
+    void Rank()  //현재점수와 랭킹점수 비교 및 배치 함수
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (int.Parse(myScore.text) > savedRankScore[i]) //i+1등보다 점수가 높으면
+            {
+                savedRankID[i + 2] = savedRankID[i + 1];
+                savedRankScore[i + 2] = savedRankScore[i + 1];
+                savedRankID[i + 1] = savedRankID[i];
+                savedRankScore[i + 1] = savedRankScore[i];
+                savedRankID[i] = myID.text;
+                savedRankScore[i] = int.Parse(myScore.text);
+            }
+        }
+    }
+
+    void RankSave()   //종료전 랭킹 점수 저장
+    {
+        // PlayerPrefs.SetInt("1st", Score_first_i);
+        // PlayerPrefs.SetInt("2nd", Score_second_i);
+        // PlayerPrefs.SetInt("3rd", Score_third_i);
     }
 }
