64aa3d7a6e45029f88ca2e7fc41f8fcd9504506d
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2024-11-21 15:31:15 +0900

    stage enum 추가

---
+++ Assets/Scripts/Enums/Stage.cs
@@ -0,0 +1,6 @@
+public enum Stage
+{
+    Null,
+    Supernatural,
+    Ditto
+}
--- Assets/Scripts/LeaderBoardManager.cs
+++ Assets/Scripts/LeaderBoardManager.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 
 public class LeaderBoardManager : MonoBehaviour
 {
+    [Header("Stage Info")]
+    public GameObject stageText;
+    private TextMeshProUGUI currentStage; //현재 스테이지
+    Stage stage = (Stage)GameManager.inst.GetStage();
+
+
     [Header("My Info")]
     public GameObject myScoreText;
     private TextMeshProUGUI myScore; //현재 점수
@@ -26,6 +32,7 @@ public class LeaderBoardManager : MonoBehaviour
     {
         myScore = myScoreText.GetComponent<TextMeshProUGUI>();
         myID = myIDText.GetComponent<TextMeshProUGUI>();
+        currentStage = stageText.GetComponent<TextMeshProUGUI>();
         for (int i = 0; i < rankIDText.Length; i++)
         {
             rankID[i] = rankIDText[i].GetComponent<TextMeshProUGUI>();
@@ -44,31 +51,31 @@ public class LeaderBoardManager : MonoBehaviour
         // 디버그용
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_1stScore");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_1stID");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_2ndScore");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_2ndID");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_3rdScore");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_3rdID");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_4thScore");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_4thID");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_5thScore");
-            PlayerPrefs.DeleteKey(GameManager.inst.GetStage().ToString() + "_5thID");
+            PlayerPrefs.DeleteKey(stage + "_1stScore");
+            PlayerPrefs.DeleteKey(stage + "_1stID");
+            PlayerPrefs.DeleteKey(stage + "_2ndScore");
+            PlayerPrefs.DeleteKey(stage + "_2ndID");
+            PlayerPrefs.DeleteKey(stage + "_3rdScore");
+            PlayerPrefs.DeleteKey(stage + "_3rdID");
+            PlayerPrefs.DeleteKey(stage + "_4thScore");
+            PlayerPrefs.DeleteKey(stage + "_4thID");
+            PlayerPrefs.DeleteKey(stage + "_5thScore");
+            PlayerPrefs.DeleteKey(stage + "_5thID");
         }
     }
 
     void ImportRank()   //랭킹 점수 불러오기
     {
-        savedRankScore[0] = PlayerPrefs.GetInt(GameManager.inst.GetStage().ToString() + "_1stScore");
-        savedRankID[0] = PlayerPrefs.GetString(GameManager.inst.GetStage().ToString() + "_1stID");
-        savedRankScore[1] = PlayerPrefs.GetInt(GameManager.inst.GetStage().ToString() + "_2ndScore");
-        savedRankID[1] = PlayerPrefs.GetString(GameManager.inst.GetStage().ToString() + "_2ndID");
-        savedRankScore[2] = PlayerPrefs.GetInt(GameManager.inst.GetStage().ToString() + "_3rdScore");
-        savedRankID[2] = PlayerPrefs.GetString(GameManager.inst.GetStage().ToString() + "_3rdID");
-        savedRankScore[3] = PlayerPrefs.GetInt(GameManager.inst.GetStage().ToString() + "_4thScore");
-        savedRankID[3] = PlayerPrefs.GetString(GameManager.inst.GetStage().ToString() + "_4thID");
-        savedRankScore[4] = PlayerPrefs.GetInt(GameManager.inst.GetStage().ToString() + "_5thScore");
-        savedRankID[4] = PlayerPrefs.GetString(GameManager.inst.GetStage().ToString() + "_5thID");
+        savedRankScore[0] = PlayerPrefs.GetInt(stage + "_1stScore");
+        savedRankID[0] = PlayerPrefs.GetString(stage + "_1stID");
+        savedRankScore[1] = PlayerPrefs.GetInt(stage + "_2ndScore");
+        savedRankID[1] = PlayerPrefs.GetString(stage + "_2ndID");
+        savedRankScore[2] = PlayerPrefs.GetInt(stage + "_3rdScore");
+        savedRankID[2] = PlayerPrefs.GetString(stage + "_3rdID");
+        savedRankScore[3] = PlayerPrefs.GetInt(stage + "_4thScore");
+        savedRankID[3] = PlayerPrefs.GetString(stage + "_4thID");
+        savedRankScore[4] = PlayerPrefs.GetInt(stage + "_5thScore");
+        savedRankID[4] = PlayerPrefs.GetString(stage + "_5thID");
         myScore.text = GameManager.inst.GetScore().ToString();
         myID.text = GameManager.inst.GetPlayerName();
     }
@@ -93,6 +100,7 @@ public class LeaderBoardManager : MonoBehaviour
 
     void ShowRank()   //화면에 랭킹 개시
     {
+        currentStage.text = stage.ToString() + " Ranking";
         for (int i = 0; i < rankScore.Length; i++)
         {
             rankID[i].text = savedRankID[i];
@@ -102,15 +110,15 @@ public class LeaderBoardManager : MonoBehaviour
 
     void RankSave()   //랭킹 점수 저장
     {
-        PlayerPrefs.SetInt(GameManager.inst.GetStage().ToString() + "_1stScore", savedRankScore[0]);
-        PlayerPrefs.SetString(GameManager.inst.GetStage().ToString() + "_1stID", savedRankID[0]);
-        PlayerPrefs.SetInt(GameManager.inst.GetStage().ToString() + "_2ndScore", savedRankScore[1]);
-        PlayerPrefs.SetString(GameManager.inst.GetStage().ToString() + "_2ndID", savedRankID[1]);
-        PlayerPrefs.SetInt(GameManager.inst.GetStage().ToString() + "_3rdScore", savedRankScore[2]);
-        PlayerPrefs.SetString(GameManager.inst.GetStage().ToString() + "_3rdID", savedRankID[2]);
-        PlayerPrefs.SetInt(GameManager.inst.GetStage().ToString() + "_4thScore", savedRankScore[3]);
-        PlayerPrefs.SetString(GameManager.inst.GetStage().ToString() + "_4thID", savedRankID[3]);
-        PlayerPrefs.SetInt(GameManager.inst.GetStage().ToString() + "_5thScore", savedRankScore[4]);
-        PlayerPrefs.SetString(GameManager.inst.GetStage().ToString() + "_5thID", savedRankID[4]);
+        PlayerPrefs.SetInt(stage + "_1stScore", savedRankScore[0]);
+        PlayerPrefs.SetString(stage + "_1stID", savedRankID[0]);
+        PlayerPrefs.SetInt(stage + "_2ndScore", savedRankScore[1]);
+        PlayerPrefs.SetString(stage + "_2ndID", savedRankID[1]);
+        PlayerPrefs.SetInt(stage + "_3rdScore", savedRankScore[2]);
+        PlayerPrefs.SetString(stage + "_3rdID", savedRankID[2]);
+        PlayerPrefs.SetInt(stage + "_4thScore", savedRankScore[3]);
+        PlayerPrefs.SetString(stage + "_4thID", savedRankID[3]);
+        PlayerPrefs.SetInt(stage + "_5thScore", savedRankScore[4]);
+        PlayerPrefs.SetString(stage + "_5thID", savedRankID[4]);
     }
 }
