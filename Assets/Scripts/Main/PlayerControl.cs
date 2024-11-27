cb6fbbab993490f5fc1a75f076fa158f46c8df5f
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2024-11-27 17:59:44 +0900

    Blink 버그 수정

--- Assets/Scripts/Main/PlayerControl.cs
+++ Assets/Scripts/Main/PlayerControl.cs
@@ -250,6 +250,7 @@ public class PlayerControl : MonoBehaviour
     // 피격 시 깜빡임
     public IEnumerator Blink()
     {
+        yield return new WaitForSeconds(0.1f);
         isBlinking = true;
         for (int i = 0; i < blinkCount; i++)
         {
@@ -334,7 +335,7 @@ public class PlayerControl : MonoBehaviour
     {
         return isInvincible;
     }
-    
+
     public bool GetIsBlinking()
     {
         return isBlinking;
