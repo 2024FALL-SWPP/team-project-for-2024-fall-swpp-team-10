323a655cd7dcaf71f214c9351a01ced3660422d2
Minjoon Choi <miles@dunamu.com>
2024-11-30 22:20:40 +0900

    init Tests directory and assembly definition structure

---
+++ Assets/Tests/GameManagerTest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GameManagerTest
+{
+    private GameManager gameManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Initialize a new instance of GameManager before each test
+        gameManager = GameManager.inst;
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void GameManagerTestSimplePasses()
+    {
+        Assert.AreEqual(true, true);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator GameManagerTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
