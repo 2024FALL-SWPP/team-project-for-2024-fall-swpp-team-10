e7d5d096d503de00321b8ed11b950dd83cf64dd4
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2024-12-03 01:17:00 +0900

    주석 추가

--- Assets/Tests/PlayMode/MainStageManagerTest.cs
+++ Assets/Tests/PlayMode/MainStageManagerTest.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
+// with GPT
 public class MainStageManagerTest
 {
     private MainStageManager mainStageManager;
