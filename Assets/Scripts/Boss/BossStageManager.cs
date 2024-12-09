42a734d6022e2bc740148fe157aa244591bcedbd
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2024-12-09 22:46:33 +0900

    위치 전부 리더보드로 통일

--- Assets/Scripts/Boss/BossStageManager.cs
+++ Assets/Scripts/Boss/BossStageManager.cs
@@ -189,11 +189,6 @@ public class BossStageManager : StageManager
     protected override IEnumerator AddScoreBasedOnLives()
     {
         StartCoroutine(base.AddScoreBasedOnLives());
-        Debug.Log("Life : " + GameManager.inst.GetLife());
-        if (GameManager.inst.GetLife() > 4)
-        {
-            GameManager.inst.SetPlayerUnlockPrefs(4);
-        }
         yield return null;
     }
 }
